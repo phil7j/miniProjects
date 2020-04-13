1b996246c657f839109cce36db9a183e5d7c4c87
Philip Johnson <phil7j@gmail.com>
2020-04-13 16:38:53 -0700

    learned how to use PHP functions and if statements

--- www/site.php
+++ www/site.php
@@ -6,16 +6,18 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="site.php" method="post">
-    Apples:<input type="checkbox" name="fruits[]" value="apples"><br>
-    Oranges:<input type="checkbox" name="fruits[]" value="oranges"><br>
-    Pears:<input type="checkbox" name="fruits[]" value="pears"><br>
-    <input type="submit">
-    </form>
+
 
     <?php
-    $grades = array("Jim"=>"A+", "Pam"=>"B-","Oscar"=>"C+");
-    echo $grades["Pam"];
+    $isMale = true;
+    $isTall = false;
+    if ($isMale && $isTall){
+       echo "ITS TRUE! You are a tall man!";
+    } elseif($isMale && !$isTall){
+        echo "Is a Man but not Tall!";
+    } else {
+        echo "ITS NOT";
+    }
     ?>
 </body>
 </html>
